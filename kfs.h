38b5a275960156ef79f29cc16f5aa7d1efa015b2
wellsilver <wellsilver2016@gmail.com>
2023-12-18 20:57:33 -0500

    minor

--- kfs.h
+++ kfs.h
@@ -10,54 +10,55 @@ struct kfs_bootsec {
 };
 
 struct kfs_folder {
+  // blank. this is a table, however all entrys are this size
   struct {
     char type;
     long long x;
     long long y;
     long long z;
-  } entries; // blank. this is a table, all entrys are this size however.
+  } entries;
 };
 
 struct kfs_folder1entry {
   char type; // uint8_t type=1
-  long long back; // uint64_t previous sector
-  long long next; // uint64_t next sector
-  long long name; // uint64_t sector where the folders name is
+  unsigned long long back; // uint64_t previous sector
+  unsigned long long next; // uint64_t next sector
+  unsigned long long name; // uint64_t sector where the folders name is
 };
 
 struct kfs_folder2entry {
   char type; // uint8_t type=2
-  long long sector; // uint64_t sector of file descriptor sector
+  unsigned long long sector; // uint64_t sector of file descriptor sector
   char hash[16]; // cityhash128 hash of file descriptor sector
 };
 
 struct kfs_folder3entry {
   char type; // uint8_t type=3
-  long long sector; // uint64_t sector of first directory sector
+  unsigned long long sector; // uint64_t sector of first directory sector
   char hash[16]; // cityhash128 hash of first sector of directory
 };
 
 struct kfs_file {
   char name[40]; // string name
-  long long creation; // uint64_t epoch creation
-  long long lastmodification; // uint64_t epoch last modification
-  long long lastread; // uint64_t epoch last read
-  long long size; // uint64_t size in bytes
-  char compression; // compression type
-  char encryption; // encryption type
+  unsigned long long creation; // uint64_t epoch creation
+  unsigned long long lastmodification; // uint64_t epoch last modification
+  unsigned long long lastread; // uint64_t epoch last read
+  unsigned long long size; // uint64_t size in bytes
+  unsigned char compression; // compression type
+  unsigned char encryption; // encryption type
   struct kfs_fileentry entries[13];
 };
 
 struct kfs_filenext {
-  long long blank;
-  long long next;
-  char hash[16];
+  unsigned long long blank; // uint64_t set to 0
+  unsigned long long next; // uint64_t the next sector
+  char hash[16]; // cityhash128 hash of the next sector
 };
 
 struct kfs_fileentry {
-  long long start;
-  long long end;
-  char hash[16];
+  unsigned long long start; // uint64_t start sector
+  unsigned long long end; // uint64_t end sector
+  char hash[16]; // cityhash128 hash of data
 };
 
 // A dependencyless implementation of google's cityhash128. out is the uint128_t
