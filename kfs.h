d0be6c5863316f68760332322f19a5c275b266f2
wellsilver <wellsilver2016@gmail.com>
2025-08-02 13:37:01 -0700

    packed attribute

--- kfs.h
+++ kfs.h
@@ -7,45 +7,45 @@ struct kfs_bootsec {
   char kfs[3]; // string "KFS"
   short ver; // uint16_t version = 2
   long long highlightedfile; // uint64_t sector of the file descriptor of a highlighted file
-};
+} __attribute__((packed));
 
 struct kfs_folder {
   char type;
   long long x;
   long long y;
   long long z;
-};
+} __attribute__((packed));
 
 struct kfs_folder1entry {
   char type; // uint8_t type=1
   unsigned long long back; // uint64_t previous sector
   unsigned long long next; // uint64_t next sector
   unsigned long long name; // uint64_t sector where the folders name is
-};
+} __attribute__((packed));
 
 struct kfs_folder2entry {
   char type; // uint8_t type=2
   unsigned long long sector; // uint64_t sector of file descriptor sector
   char hash[16]; // cityhash128 hash of file descriptor sector
-};
+} __attribute__((packed));
 
 struct kfs_folder3entry {
   char type; // uint8_t type=3
   unsigned long long sector; // uint64_t sector of first directory sector
   char hash[16]; // cityhash128 hash of first sector of directory
-};
+} __attribute__((packed));
 
 struct kfs_filenext {
   unsigned long long _; // uint64_t set to 0
   unsigned long long next; // uint64_t the next sector
   char hash[16]; // cityhash128 hash of the next sector
-};
+} __attribute__((packed));
 
 struct kfs_fileentry {
   unsigned long long start; // uint64_t start sector
   unsigned long long end; // uint64_t end sector
   char hash[16]; // cityhash128 hash of data
-};
+} __attribute__((packed));
 
 struct kfs_file {
   char name[40]; // string name
@@ -56,7 +56,7 @@ struct kfs_file {
   unsigned char compression; // compression type
   unsigned char encryption; // encryption type
   struct kfs_fileentry entries[13];
-};
+} __attribute__((packed));
 
 // A dependencyless implementation of google's cityhash128. out is the uint128_t
 void kfscityhash128(char *in, int size, char out[16]) {
